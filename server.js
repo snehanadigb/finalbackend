c8820c22d20a8f31fd8b5ff5678d7b07dc197c7b
B <SnehaNadig.B@team.telstra.com>
2024-09-25 15:16:55 +0530

    lastfirst4

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
-
+const app = express();
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
     console.log(`Authentication service running on port ${process.env.PORT}`);
